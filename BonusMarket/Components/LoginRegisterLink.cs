cb66a366557d78f6461e77989f3ec10b5cdd74ce
artmelkonyan <arthurmelkonyan@gmail.com>
2023-08-24 23:19:51 +0400

    Added account login button

---
+++ BonusMarket/Components/LoginRegisterLink.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BonusMarket.Components
+{
+    public class LoginRegisterLinkViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(bool isMobile = false)
+        {
+            ViewBag.IsMobile = isMobile;
+            return View();
+        }
+    }
+}
