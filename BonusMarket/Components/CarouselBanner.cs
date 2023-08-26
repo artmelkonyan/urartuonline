5d3a148839544d3f98ceeedf04c35e8c154aadfa
artmelkonyan <arthurmelkonyan@gmail.com>
2023-08-27 00:29:46 +0400

    Added Home baner

---
+++ BonusMarket/Components/CarouselBanner.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BonusMarket.Components
+{
+    public class CarouselBannerViewComponent : ViewComponent
+    {
+        PictureLayer p_layer;
+        BannerLayer b_layer;
+        public CarouselBannerViewComponent()
+        {
+            p_layer = new PictureLayer();
+            b_layer = new BannerLayer();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var banners = b_layer.GetAll().Where(x => x.Show).OrderBy(x => x.OrderId).ToList();
+            foreach (var item in banners)
+            {
+                item.Picture = p_layer.GetPictureById(item.PictureId);
+            }
+
+            return View(banners);
+        }
+    }
+}
