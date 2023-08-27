30dbf294b5da6803246c2916b0ee6f51e8b3f54a
artmelkonyan <arthurmelkonyan@gmail.com>
2023-08-27 14:38:56 +0400

    Added HomePageNewProducts

---
+++ BonusMarket/Components/HomePageNewProducts.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+
+namespace BonusMarket.Components
+{
+    public class HomePageNewProductsViewComponent : ViewComponent
+    {
+        protected ProductLayer p_layer;
+        protected string requestLanguage;
+
+        public HomePageNewProductsViewComponent()
+        {
+            requestLanguage = Thread.CurrentThread.CurrentUICulture.Name;
+            p_layer = new ProductLayer();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = p_layer.GetHomePageProducts(requestLanguage).Where(x => x.Count > 0).ToList();
+            return View(result);
+        }
+    }
+}
--- Models/GeneralHelper.cs
+++ Models/GeneralHelper.cs
@@ -11,5 +11,8 @@ namespace Models
             string priceString = Price.ToString();
             return priceString.Substring(0, priceString.Length - 2);
         }
+
+        public static string GetDiscountPercent(decimal price, decimal oldPrice)
+            => Math.Floor(((oldPrice - price) / oldPrice) * 100).ToString();
     }
 }
