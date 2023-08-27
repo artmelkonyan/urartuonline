8a27601b1e349849c37536bd6c639305cb02b709
artmelkonyan <arthurmelkonyan@gmail.com>
2023-08-27 15:51:56 +0400

    Added home page brands

---
+++ BonusMarket/Components/HomePageBrands.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BonusMarket.Components
+{
+    public class HomePageBrandsViewComponent : BaseViewComponent
+    {
+        BrandLayer brandLayer;
+
+
+        public HomePageBrandsViewComponent()
+        {
+            brandLayer = new BrandLayer();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var brands = brandLayer.GetOnHomePage(RequestLanguage).ToList();
+            return View(brands);
+        }
+    }
+}
